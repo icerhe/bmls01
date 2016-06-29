58f6f529ed5cfbb81450ada5baf896578b69c934
icerhe <icerhe@gmail.com>
2016-06-29 18:34:20 +0800

    111

--- my01.py
+++ my01.py
@@ -1,30 +1,32 @@
-#my first machine learning code
+# my first machine learning code
 
 import os
 import scipy as sp
 import matplotlib.pyplot as plt
 
-class Scatter:
 
+class Scatter:
     dataDir = os.path.join(
         os.path.dirname(os.path.realpath(__file__)), "data")
 
     chartDir = os.path.join(
         os.path.dirname(os.path.realpath(__file__)), "charts")
 
+    def gen_char01(self):
+        # load file
+        data = Scatter.load_csv("data.csv")
+        x = data[:, 0]
+        y = data[:, 1]
+        print(x,y)
 
-    def genChart01(self):
-        #load file
-        data = self.loadCsv('data.csv')
-        print(data)
 
 
+    @staticmethod
+    def load_csv(csv_file_name):
+        return sp.genfromtxt(os.path.join(Scatter.dataDir, csv_file_name), delimiter=",")
 
-
-    def loadCsv(self,csvFileName):
-        data = sp.genfromtxt(os.path.join(Scatter.dataDir, csvFileName), delimiter=",")
-        return data
-
+    def plot_models(self, x, y, models, outfile, mx=None, y_max=None, x_min=None):
+        plt.figure(num=None, figsize=(8,6))
 
 sc = Scatter()
-sc.genChart01()
+sc.gen_char01()
